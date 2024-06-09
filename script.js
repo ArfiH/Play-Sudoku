e3d1975a904f12a402663a6e1e0cf207db5a31f7
ArfiH <arfihussain27@gmail.com>
2024-06-09 22:30:35 +0530

    added Check game status :sparkles:

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const board = document.querySelector(".board");
+const checkBtn = document.querySelector(".check-btn");
+const dialPad = document.querySelector(".dial-pad");
 let selectedCell = null;
 let playboard = [];
+let solutionBoard = [];
 
 async function fetchBoard(difficulty) {
   try {
     const response = await fetch(
-      `https://sugoku.onrender.com/board?difficulty=${difficulty}`
+      `https://sudoku-api.vercel.app/api/dosuku?query={newboard(limit:1){grids{value,solution,difficulty}}}`
     );
     const data = await response.json();
-    playboard = [...data.board];
-    console.log(`Play Board is ${playboard}`);
-
-    return data.board;
+    playboard = [...data.newboard.grids[0].value];
+    solutionBoard = [...data.newboard.grids[0].solution];
+    console.log(data.newboard.grids[0].difficulty);
+    console.log(solutionBoard);
+    
+    return data.newboard.grids[0].value;
   } catch (error) {
     console.error("Error:", error);
   }
@@ -56,18 +61,44 @@ board.addEventListener("keydown", (event) => {
   if (selectedCell && event.key >= "1" && event.key <= "9") {
     event.preventDefault();
     selectedCell.textContent = event.key;
+    playboard[selectedCell.getAttribute("data-row")][
+      selectedCell.getAttribute("data-col")
+    ] = Number(event.key);
     selectedCell.classList.remove("selected");
     selectedCell.contentEditable = false;
     selectedCell = null;
   }
 });
 
-const dialPad = document.querySelector(".dial-pad");
-
 dialPad.addEventListener("click", (event) => {
   if (selectedCell && event.target.tagName === "BUTTON") {
     selectedCell.textContent = event.target.textContent;
+    playboard[selectedCell.getAttribute("data-row")][
+      selectedCell.getAttribute("data-col")
+    ] = Number(event.target.textContent);
+    console.log(`Play Board is ${playboard}`);
     selectedCell.classList.remove("selected");
     selectedCell = null;
   }
+});
+
+checkBtn.addEventListener("click", () => {
+  let correct = true;
+  if (playboard.length != solutionBoard.length) {
+    correct = false;
+  } else {
+    for (let i = 0; i < playboard.length; i++) {
+      if (playboard[i] !== solutionBoard[i]) {
+        correct = false;
+        break;
+      }
+    }
+  }
+
+  if (correct) {
+    console.log("hurray");
+  } else {
+    console.log("lost");
+  }
+  console.log(playboard)
 });
